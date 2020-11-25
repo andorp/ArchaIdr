6209c6bd47db8110fdbd978f7d3d0a44acce43d3
Andor Penzes <andor.penzes@gmail.com>
2020-11-25 23:48:53 +0100

    Pong: jumping ball.

--- Pong/game/pong.js
+++ Pong/game/pong.js
@@ -5,12 +5,54 @@ const __esPrim_crashExp = (x=>{throw new IdrisError(x)});
 const __esPrim_idrisworld = (Symbol('idrisworld'));
 const __esPrim_int_bound_63 = (BigInt(2) ** BigInt(63) );
 
-function Main_GameLoop_updateGameState(arg_0, ext_0){//src/Main.idr:78:3--79:18
+function Main_GameLogic_case__moveBall$2cdy$27_492(arg_0, arg_1, arg_2, arg_3, arg_4){//src/Main.idr:107:13--107:57
+ switch(arg_4){
+  case 0n: {
+   return ((Prelude_Num_negate$5fNeg$5f$5fInt((1n % __esPrim_int_bound_63)) * arg_2) % __esPrim_int_bound_63);
+   break; }
+  case 1n: {
+   return arg_2;
+   break; }
+  default:
+   throw new Error('unhandled const case on :1:1--1:1');
+ }
+}
+
+function Main_GameLogic_case__moveBall$2cdx$27_453(arg_0, arg_1, arg_2, arg_3, arg_4){//src/Main.idr:104:13--104:57
+ switch(arg_4){
+  case 0n: {
+   return ((Prelude_Num_negate$5fNeg$5f$5fInt((1n % __esPrim_int_bound_63)) * arg_3) % __esPrim_int_bound_63);
+   break; }
+  case 1n: {
+   return arg_3;
+   break; }
+  default:
+   throw new Error('unhandled const case on :1:1--1:1');
+ }
+}
+
+function Main_GameLogic_n__1773_432_y$27(arg_0, arg_1, arg_2, arg_3){//src/Main.idr:100:7--101:9
+ return ((arg_0 + arg_2) % __esPrim_int_bound_63);
+}
+
+function Main_GameLogic_n__1773_431_x$27(arg_0, arg_1, arg_2, arg_3){//src/Main.idr:97:7--98:9
+ return ((arg_1 + arg_3) % __esPrim_int_bound_63);
+}
+
+function Main_GameLogic_n__1773_434_dy$27(arg_0, arg_1, arg_2, arg_3){//src/Main.idr:106:7--107:10
+ return Main_GameLogic_case__moveBall$2cdy$27_492(arg_0, arg_1, arg_2, arg_3, Prelude_Basics_$7c$7c(Prelude_EqOrd_$3e$5fOrd$5f$5fInt(Main_GameLogic_n__1773_432_y$27(arg_0, arg_1, arg_2, arg_3), (600n % __esPrim_int_bound_63)), (()=>Prelude_EqOrd_$3c$5fOrd$5f$5fInt(Main_GameLogic_n__1773_432_y$27(arg_0, arg_1, arg_2, arg_3), (0n % __esPrim_int_bound_63)))));
+}
+
+function Main_GameLogic_n__1773_433_dx$27(arg_0, arg_1, arg_2, arg_3){//src/Main.idr:103:7--104:10
+ return Main_GameLogic_case__moveBall$2cdx$27_453(arg_0, arg_1, arg_2, arg_3, Prelude_Basics_$7c$7c(Prelude_EqOrd_$3e$5fOrd$5f$5fInt(Main_GameLogic_n__1773_431_x$27(arg_0, arg_1, arg_2, arg_3), (800n % __esPrim_int_bound_63)), (()=>Prelude_EqOrd_$3c$5fOrd$5f$5fInt(Main_GameLogic_n__1773_431_x$27(arg_0, arg_1, arg_2, arg_3), (0n % __esPrim_int_bound_63)))));
+}
+
+function Main_GameLoop_updateGameState(arg_0, ext_0){//src/Main.idr:133:3--134:18
  const imp$5fgen_0 = Main_GameLoop_getGameState(ext_0);
  return Main_GameLoop_setGameState(arg_0(imp$5fgen_0), ext_0);
 }
 
-function Main_Window_setInterval(arg_0, arg_1, arg_2, arg_3){//src/Main.idr:33:3--35:14
+function Main_Window_setInterval(arg_0, arg_1, arg_2, arg_3){//src/Main.idr:68:3--70:14
  switch(arg_1.h){
   case 0: {
    return arg_1.a2(undefined)((eta_0) => {return Main_Window_prim$5f$5fsetInterval(arg_2, arg_3, eta_0);});
@@ -20,7 +62,7 @@ function Main_Window_setInterval(arg_0, arg_1, arg_2, arg_3){//src/Main.idr:33:3
  }
 }
 
-function Main_GameLoop_setGameState(arg_0, ext_0){//src/Main.idr:69:3--70:15
+function Main_GameLoop_setGameState(arg_0, ext_0){//src/Main.idr:124:3--125:15
  return Main_GameLoop_prim$5f$5fsetGameState(arg_0, ext_0);
 }
 
@@ -33,84 +75,155 @@ const Main_GameLoop_prim$5f$5fsetGameState = ((s)=>{gameState=s;})
 const Main_GameLoop_prim$5f$5fgetGameState = (()=>{return gameState;})
 
 
-const Main_Window_prim$5f$5ffillStyle = ((s)=>canvasContext.fillStyle=s)
+const Main_Canvas_prim$5f$5ffillStyle = ((s)=>canvasContext.fillStyle=s)
 
 
-const Main_Window_prim$5f$5ffillRect = ((x,y,w,h)=>canvasContext.fillRect(Number(x),Number(y),Number(w),Number(h)))
+const Main_Canvas_prim$5f$5ffillRect = ((x,y,w,h)=>canvasContext.fillRect(Number(x),Number(y),Number(w),Number(h)))
 
 
-function Main_main(ext_0){//src/Main.idr:98:1--99:5
- return Main_GameLoop_gameLoop(30n, Main_GameLogic_initBall(), (eta_0) => {return Main_GameLogic_incBall(eta_0);}, (eta_0) => {return (eta_1) => {return Main_GameLogic_drawBall(eta_0, eta_1);};}, ext_0);
+const Main_Canvas_prim$5f$5ffill = (()=>canvasContext.fill())
+
+
+const Main_Console_prim$5f$5fconsoleLog = ((msg)=>console.log(msg))
+
+
+const Main_Canvas_prim$5f$5fbeginPath = (()=>canvasContext.beginPath())
+
+
+const Main_Canvas_prim$5f$5farc = ((x,y,r,s,e,b)=>canvasContext.arc(Number(x),Number(y),Number(r),Number(s),Number(e),true))
+
+
+function Main_GameLogic_moveBall(arg_0){//src/Main.idr:92:3--94:11
+ switch(arg_0.h){
+  case 0: {
+   let imp$5fgen_1;
+   switch(arg_0.a1.h){
+    case 0: {
+     let imp$5fgen_2;
+     switch(arg_0.a2.h){
+      case 0: {
+       imp$5fgen_2 = ({h:0, a1: ({h:0, a1: Main_GameLogic_n__1773_431_x$27(arg_0.a1.a2, arg_0.a1.a1, arg_0.a2.a2, arg_0.a2.a1), a2: Main_GameLogic_n__1773_432_y$27(arg_0.a1.a2, arg_0.a1.a1, arg_0.a2.a2, arg_0.a2.a1)}), a2: ({h:0, a1: Main_GameLogic_n__1773_433_dx$27(arg_0.a1.a2, arg_0.a1.a1, arg_0.a2.a2, arg_0.a2.a1), a2: Main_GameLogic_n__1773_434_dy$27(arg_0.a1.a2, arg_0.a1.a1, arg_0.a2.a2, arg_0.a2.a1)})});
+       break; }
+      default:
+       throw new Error('unhandled con case on src/Main.idr:86:23--86:31');
+     }
+     imp$5fgen_1 = imp$5fgen_2;
+     break; }
+    default:
+     throw new Error('unhandled con case on src/Main.idr:86:15--86:19');
+   }
+   return imp$5fgen_1;
+   break; }
+  default:
+   throw new Error('unhandled con case on src/Main.idr:93:21--93:26');
+ }
 }
 
-function Main_GameLogic_initBall(){//src/Main.idr:48:3--50:11
- return 1n;
+function Main_main(ext_0){//src/Main.idr:153:1--154:5
+ Main_Console_consoleLog(undefined, ({h:0, a1: ({h:0, a1: ({h:0, a1: (b) => {return (a) => {return (func) => {return (arg_149) => {return (eta_0) => {return Prelude_IO_map$5fFunctor$5f$5fIO(undefined, undefined, func, arg_149, eta_0);};};};};}, a2: (a) => {return (arg_482) => {return (eta_0) => {return arg_482;};};}, a3: (b) => {return (a) => {return (arg_483) => {return (arg_485) => {return (eta_0) => {const imp$5fgen_4 = arg_483(eta_0);
+const imp$5fgen_3 = arg_485(eta_0);
+return imp$5fgen_4(imp$5fgen_3);};};};};}}), a2: (b) => {return (a) => {return (arg_644) => {return (arg_645) => {return (eta_0) => {const imp$5fgen_5 = arg_644(eta_0);
+return arg_645(imp$5fgen_5)(eta_0);};};};};}, a3: (a) => {return (arg_647) => {return (eta_0) => {const imp$5fgen_6 = arg_647(eta_0);
+return imp$5fgen_6(eta_0);};};}}), a2: (a) => {return (arg_6794) => {return arg_6794;};}}), 'Starting...')(ext_0);
+ return Main_GameLoop_gameLoop(30n, Main_GameLogic_initBall(), (eta_0) => {return Main_GameLogic_moveBall(eta_0);}, (eta_0) => {return Main_GameLogic_drawBall(eta_0);}, ext_0);
 }
 
-function Main_GameLogic_incBall(arg_0){//src/Main.idr:52:3--54:10
- return ((arg_0 + 2n) % __esPrim_int_bound_63);
+function Main_GameLogic_initBall(){//src/Main.idr:88:3--90:11
+ return ({h:0, a1: ({h:0, a1: 0n, a2: 0n}), a2: ({h:0, a1: 5n, a2: 5n})});
 }
 
-function Main_GameLoop_getGameState(ext_0){//src/Main.idr:75:3--76:15
+function Main_GameLoop_getGameState(ext_0){//src/Main.idr:130:3--131:15
  return Main_GameLoop_prim$5f$5fgetGameState(ext_0);
 }
 
-function Main_GameLoop_gameLoop(arg_0, arg_1, arg_2, arg_3, ext_0){//src/Main.idr:88:3--90:11
+function Main_GameLoop_gameLoop(arg_0, arg_1, arg_2, arg_3, ext_0){//src/Main.idr:143:3--145:11
  Main_GameLoop_setGameState(arg_1, ext_0);
- return Main_Window_setInterval(undefined, ({h:0, a1: ({h:0, a1: ({h:0, a1: (b) => {return (a) => {return (func) => {return (arg_149) => {return (eta_0) => {return Prelude_IO_map$5fFunctor$5f$5fIO(undefined, undefined, func, arg_149, eta_0);};};};};}, a2: (a) => {return (arg_482) => {return (eta_0) => {return arg_482;};};}, a3: (b) => {return (a) => {return (arg_483) => {return (arg_485) => {return (eta_0) => {const imp$5fgen_2 = arg_483(eta_0);
-const imp$5fgen_1 = arg_485(eta_0);
-return imp$5fgen_2(imp$5fgen_1);};};};};}}), a2: (b) => {return (a) => {return (arg_644) => {return (arg_645) => {return (eta_0) => {const imp$5fgen_3 = arg_644(eta_0);
-return arg_645(imp$5fgen_3)(eta_0);};};};};}, a3: (a) => {return (arg_647) => {return (eta_0) => {const imp$5fgen_4 = arg_647(eta_0);
-return imp$5fgen_4(eta_0);};};}}), a2: (a) => {return (arg_6794) => {return arg_6794;};}}), (eta_0) => {Main_GameLoop_updateGameState(arg_2, eta_0);
+ return Main_Window_setInterval(undefined, ({h:0, a1: ({h:0, a1: ({h:0, a1: (b) => {return (a) => {return (func) => {return (arg_149) => {return (eta_0) => {return Prelude_IO_map$5fFunctor$5f$5fIO(undefined, undefined, func, arg_149, eta_0);};};};};}, a2: (a) => {return (arg_482) => {return (eta_0) => {return arg_482;};};}, a3: (b) => {return (a) => {return (arg_483) => {return (arg_485) => {return (eta_0) => {const imp$5fgen_8 = arg_483(eta_0);
+const imp$5fgen_7 = arg_485(eta_0);
+return imp$5fgen_8(imp$5fgen_7);};};};};}}), a2: (b) => {return (a) => {return (arg_644) => {return (arg_645) => {return (eta_0) => {const imp$5fgen_9 = arg_644(eta_0);
+return arg_645(imp$5fgen_9)(eta_0);};};};};}, a3: (a) => {return (arg_647) => {return (eta_0) => {const imp$5fgen_10 = arg_647(eta_0);
+return imp$5fgen_10(eta_0);};};}}), a2: (a) => {return (arg_6794) => {return arg_6794;};}}), (eta_0) => {Main_GameLoop_updateGameState(arg_2, eta_0);
 return Main_GameLoop_drawGameState(arg_3, eta_0);}, Prelude_Num_div$5fIntegral$5f$5fInt((1000n % __esPrim_int_bound_63), arg_0))(ext_0);
 }
 
-function Main_Window_fillStyle(arg_0, arg_1, arg_2){//src/Main.idr:19:3--21:12
+function Main_Canvas_fillStyle(arg_0, ext_0){//src/Main.idr:18:3--20:12
+ return Main_Canvas_prim$5f$5ffillStyle(arg_0, ext_0);
+}
+
+function Main_Canvas_fillRect(arg_0, arg_1, arg_2, arg_3, ext_0){//src/Main.idr:25:3--27:11
+ return Main_Canvas_prim$5f$5ffillRect(arg_0, arg_1, arg_2, arg_3, ext_0);
+}
+
+function Main_Canvas_fill(ext_0){//src/Main.idr:46:3--48:7
+ return Main_Canvas_prim$5f$5ffill(ext_0);
+}
+
+function Main_GameLoop_drawGameState(arg_0, ext_0){//src/Main.idr:138:3--139:16
+ const imp$5fgen_11 = Main_GameLoop_getGameState(ext_0);
+ return arg_0(imp$5fgen_11)(ext_0);
+}
+
+function Main_GameLogic_drawBall(arg_0){//src/Main.idr:109:3--111:11
+ switch(arg_0.h){
+  case 0: {
+   let imp$5fgen_12;
+   switch(arg_0.a1.h){
+    case 0: {
+     imp$5fgen_12 = (eta_0) => {Main_colorRect(0n, 0n, (800n % __esPrim_int_bound_63), (600n % __esPrim_int_bound_63), 'black', eta_0);
+Main_colorRect(0n, (210n % __esPrim_int_bound_63), 10n, (100n % __esPrim_int_bound_63), 'white', eta_0);
+return Main_colorCircle(arg_0.a1.a1, arg_0.a1.a2, 10n, 'white', eta_0);};
+     break; }
+    default:
+     throw new Error('unhandled con case on src/Main.idr:86:15--86:19');
+   }
+   return imp$5fgen_12;
+   break; }
+  default:
+   throw new Error('unhandled con case on src/Main.idr:110:21--110:26');
+ }
+}
+
+function Main_Console_consoleLog(arg_0, arg_1, arg_2){//src/Main.idr:9:3--11:13
  switch(arg_1.h){
   case 0: {
-   return arg_1.a2(undefined)((eta_0) => {return Main_Window_prim$5f$5ffillStyle(arg_2, eta_0);});
+   return arg_1.a2(undefined)((eta_0) => {return Main_Console_prim$5f$5fconsoleLog(arg_2, eta_0);});
    break; }
   default:
    throw new Error('unhandled con case on Prelude/IO.idr:35:3--35:32');
  }
 }
 
-function Main_Window_fillRect(arg_0, arg_1, arg_2, arg_3, arg_4, arg_5){//src/Main.idr:26:3--28:11
- switch(arg_1.h){
-  case 0: {
-   return arg_1.a2(undefined)((eta_0) => {return Main_Window_prim$5f$5ffillRect(arg_2, arg_3, arg_4, arg_5, eta_0);});
+function Main_colorRect(arg_0, arg_1, arg_2, arg_3, arg_4, ext_0){//src/Main.idr:51:1--52:10
+ Main_Canvas_fillStyle(arg_4, ext_0);
+ return Main_Canvas_fillRect(arg_0, arg_1, arg_2, arg_3, ext_0);
+}
+
+function Main_colorCircle(arg_0, arg_1, arg_2, arg_3, ext_0){//src/Main.idr:56:1--57:12
+ Main_Canvas_fillStyle(arg_3, ext_0);
+ Main_Canvas_beginPath(ext_0);
+ Main_Canvas_arc(arg_0, arg_1, arg_2, 0.0, 6.283185307179586, ext_0);
+ return Main_Canvas_fill(ext_0);
+}
+
+function Main_Canvas_beginPath(ext_0){//src/Main.idr:39:3--41:12
+ return Main_Canvas_prim$5f$5fbeginPath(ext_0);
+}
+
+function Main_Canvas_arc(arg_0, arg_1, arg_2, arg_3, arg_4, ext_0){//src/Main.idr:32:3--34:6
+ return Main_Canvas_prim$5f$5farc(arg_0, arg_1, arg_2, arg_3, arg_4, ext_0);
+}
+
+function Prelude_Basics_$7c$7c(arg_0, arg_1){//Prelude/Basics.idr:104:1--107:2
+ switch(arg_0){
+  case 0n: {
+   return 0n;
+   break; }
+  case 1n: {
+   return arg_1();
    break; }
   default:
-   throw new Error('unhandled con case on Prelude/IO.idr:35:3--35:32');
+   throw new Error('unhandled const case on Prelude/Basics.idr:106:15--106:19');
  }
-}
-
-function Main_GameLoop_drawGameState(arg_0, ext_0){//src/Main.idr:83:3--84:16
- const imp$5fgen_5 = Main_GameLoop_getGameState(ext_0);
- return arg_0(imp$5fgen_5)(ext_0);
-}
-
-function Main_GameLogic_drawBall(arg_0, ext_0){//src/Main.idr:56:3--58:11
- Main_Window_fillStyle(undefined, ({h:0, a1: ({h:0, a1: ({h:0, a1: (b) => {return (a) => {return (func) => {return (arg_149) => {return (eta_0) => {return Prelude_IO_map$5fFunctor$5f$5fIO(undefined, undefined, func, arg_149, eta_0);};};};};}, a2: (a) => {return (arg_482) => {return (eta_0) => {return arg_482;};};}, a3: (b) => {return (a) => {return (arg_483) => {return (arg_485) => {return (eta_0) => {const imp$5fgen_7 = arg_483(eta_0);
-const imp$5fgen_6 = arg_485(eta_0);
-return imp$5fgen_7(imp$5fgen_6);};};};};}}), a2: (b) => {return (a) => {return (arg_644) => {return (arg_645) => {return (eta_0) => {const imp$5fgen_8 = arg_644(eta_0);
-return arg_645(imp$5fgen_8)(eta_0);};};};};}, a3: (a) => {return (arg_647) => {return (eta_0) => {const imp$5fgen_9 = arg_647(eta_0);
-return imp$5fgen_9(eta_0);};};}}), a2: (a) => {return (arg_6794) => {return arg_6794;};}}), 'black')(ext_0);
- Main_Window_fillRect(undefined, ({h:0, a1: ({h:0, a1: ({h:0, a1: (b) => {return (a) => {return (func) => {return (arg_149) => {return (eta_0) => {return Prelude_IO_map$5fFunctor$5f$5fIO(undefined, undefined, func, arg_149, eta_0);};};};};}, a2: (a) => {return (arg_482) => {return (eta_0) => {return arg_482;};};}, a3: (b) => {return (a) => {return (arg_483) => {return (arg_485) => {return (eta_0) => {const imp$5fgen_11 = arg_483(eta_0);
-const imp$5fgen_10 = arg_485(eta_0);
-return imp$5fgen_11(imp$5fgen_10);};};};};}}), a2: (b) => {return (a) => {return (arg_644) => {return (arg_645) => {return (eta_0) => {const imp$5fgen_12 = arg_644(eta_0);
-return arg_645(imp$5fgen_12)(eta_0);};};};};}, a3: (a) => {return (arg_647) => {return (eta_0) => {const imp$5fgen_13 = arg_647(eta_0);
-return imp$5fgen_13(eta_0);};};}}), a2: (a) => {return (arg_6794) => {return arg_6794;};}}), 0n, 0n, (800n % __esPrim_int_bound_63), (600n % __esPrim_int_bound_63))(ext_0);
- Main_Window_fillStyle(undefined, ({h:0, a1: ({h:0, a1: ({h:0, a1: (b) => {return (a) => {return (func) => {return (arg_149) => {return (eta_0) => {return Prelude_IO_map$5fFunctor$5f$5fIO(undefined, undefined, func, arg_149, eta_0);};};};};}, a2: (a) => {return (arg_482) => {return (eta_0) => {return arg_482;};};}, a3: (b) => {return (a) => {return (arg_483) => {return (arg_485) => {return (eta_0) => {const imp$5fgen_15 = arg_483(eta_0);
-const imp$5fgen_14 = arg_485(eta_0);
-return imp$5fgen_15(imp$5fgen_14);};};};};}}), a2: (b) => {return (a) => {return (arg_644) => {return (arg_645) => {return (eta_0) => {const imp$5fgen_16 = arg_644(eta_0);
-return arg_645(imp$5fgen_16)(eta_0);};};};};}, a3: (a) => {return (arg_647) => {return (eta_0) => {const imp$5fgen_17 = arg_647(eta_0);
-return imp$5fgen_17(eta_0);};};}}), a2: (a) => {return (arg_6794) => {return arg_6794;};}}), 'red')(ext_0);
- return Main_Window_fillRect(undefined, ({h:0, a1: ({h:0, a1: ({h:0, a1: (b) => {return (a) => {return (func) => {return (arg_149) => {return (eta_0) => {return Prelude_IO_map$5fFunctor$5f$5fIO(undefined, undefined, func, arg_149, eta_0);};};};};}, a2: (a) => {return (arg_482) => {return (eta_0) => {return arg_482;};};}, a3: (b) => {return (a) => {return (arg_483) => {return (arg_485) => {return (eta_0) => {const imp$5fgen_19 = arg_483(eta_0);
-const imp$5fgen_18 = arg_485(eta_0);
-return imp$5fgen_19(imp$5fgen_18);};};};};}}), a2: (b) => {return (a) => {return (arg_644) => {return (arg_645) => {return (eta_0) => {const imp$5fgen_20 = arg_644(eta_0);
-return arg_645(imp$5fgen_20)(eta_0);};};};};}, a3: (a) => {return (arg_647) => {return (eta_0) => {const imp$5fgen_21 = arg_647(eta_0);
-return imp$5fgen_21(eta_0);};};}}), a2: (a) => {return (arg_6794) => {return arg_6794;};}}), arg_0, (100n % __esPrim_int_bound_63), 5n, 5n)(ext_0);
 }
 
 function Builtin_idris$5fcrash(arg_0, ext_0){//Builtin.idr:175:1--177:12
@@ -127,12 +240,36 @@ function Prelude_Num_case__div_440(arg_0, arg_1, arg_2){//Prelude/Num.idr:114:9-
  }
 }
 
+function Prelude_Num_negate$5fNeg$5f$5fInt(arg_0){//Prelude/Num.idr:102:1--107:7
+ return ((0n - arg_0) % __esPrim_int_bound_63);
+}
+
 function Prelude_Num_div$5fIntegral$5f$5fInt(arg_0, arg_1){//Prelude/Num.idr:111:1--122:8
  return Prelude_Num_case__div_440(arg_1, arg_0, Prelude_EqOrd_$3d$3d$5fEq$5f$5fInt(arg_1, (0n % __esPrim_int_bound_63)));
 }
 
+function Prelude_EqOrd_$3e$5fOrd$5f$5fInt(arg_0, arg_1){//Prelude/EqOrd.idr:128:1--137:7
+ switch(((arg_0 > arg_1) ? BigInt(1) : BigInt(0))){
+  case 0n: {
+   return 1n;
+   break; }
+  default:
+   return 0n;
+ }
+}
+
 function Prelude_EqOrd_$3d$3d$5fEq$5f$5fInt(arg_0, arg_1){//Prelude/EqOrd.idr:36:1--40:7
  switch(((arg_0 === arg_1) ? BigInt(1) : BigInt(0))){
+  case 0n: {
+   return 1n;
+   break; }
+  default:
+   return 0n;
+ }
+}
+
+function Prelude_EqOrd_$3c$5fOrd$5f$5fInt(arg_0, arg_1){//Prelude/EqOrd.idr:128:1--137:7
+ switch(((arg_0 < arg_1) ? BigInt(1) : BigInt(0))){
   case 0n: {
    return 1n;
    break; }
@@ -158,8 +295,8 @@ function PrimIO_unsafeCreateWorld(arg_0, arg_1){//PrimIO.idr:94:1--95:18
 }
 
 function Prelude_IO_map$5fFunctor$5f$5fIO(arg_0, arg_1, arg_2, arg_3, ext_0){//Prelude/IO.idr:15:1--19:8
- const imp$5fgen_22 = arg_3(ext_0);
- return arg_2(imp$5fgen_22);
+ const imp$5fgen_13 = arg_3(ext_0);
+ return arg_2(imp$5fgen_13);
 }
 
 
--- Pong/src/Main.idr
+++ Pong/src/Main.idr
@@ -10,22 +10,57 @@ namespace Console
   consoleLog : HasIO io => String -> io ()
   consoleLog msg = primIO $ prim__consoleLog msg
 
-
-namespace Window
+namespace Canvas
 
   %foreign "browser:lambda:(s)=>canvasContext.fillStyle=s"
   prim__fillStyle : String -> PrimIO ()
 
   export
-  fillStyle : HasIO io => String -> io ()
+  fillStyle : String -> IO ()
   fillStyle s = primIO $ prim__fillStyle s
 
   %foreign "browser:lambda:(x,y,w,h)=>canvasContext.fillRect(Number(x),Number(y),Number(w),Number(h))"
   prim__fillRect : Int -> Int -> Int -> Int -> PrimIO ()
 
   export
-  fillRect : HasIO io => Int -> Int -> Int -> Int -> io ()
+  fillRect : Int -> Int -> Int -> Int -> IO ()
   fillRect x y w h = primIO $ prim__fillRect x y w h
+
+  %foreign "browser:lambda:(x,y,r,s,e,b)=>canvasContext.arc(Number(x),Number(y),Number(r),Number(s),Number(e),true)"
+  prim__arc : Int -> Int -> Int -> Double -> Double -> PrimIO ()
+
+  export
+  arc : Int -> Int -> Int -> Double -> Double -> IO ()
+  arc x y r s e = primIO $ prim__arc x y r s e
+
+  %foreign "browser:lambda:()=>canvasContext.beginPath()"
+  prim__beginPath : PrimIO ()
+
+  export
+  beginPath : IO ()
+  beginPath = primIO prim__beginPath
+
+  %foreign "browser:lambda:()=>canvasContext.fill()"
+  prim__fill : PrimIO ()
+
+  export
+  fill : IO ()
+  fill = primIO prim__fill
+
+
+colorRect : Int -> Int -> Int -> Int -> String -> IO ()
+colorRect l t w h c = do
+  fillStyle c
+  fillRect l t w h
+
+colorCircle : Int -> Int -> Int -> String -> IO ()
+colorCircle x y r c = do
+  fillStyle c
+  beginPath
+  arc x y r 0.0 6.283185307179586
+  fill
+
+namespace Window
 
   %foreign "browser:lambda:(a,i)=>setInterval(a,Number(i))"
   prim__setInterval : PrimIO () -> Int -> PrimIO ()
@@ -34,32 +69,52 @@ namespace Window
   setInterval : (HasIO io) => IO () -> Int -> io ()
   setInterval a i = primIO $ prim__setInterval (toPrim a) i
 
-
 namespace GameLogic
+
+  record Ball where
+    constructor MkBall
+    X : Int
+    Y : Int
+
+  record Velocity where
+    constructor MkVelocity
+    dX : Int
+    dY : Int
 
   public export
   data State : Type where
-    MkState : Int -> State
-
-  export
-  Show State where
-    show (MkState ball) = show ball
+    MkState : Ball -> Velocity -> State
 
   export
   initBall : State
-  initBall = MkState 1
+  initBall = MkState (MkBall 0 0) (MkVelocity 5 5)
 
   export
-  incBall : (1 _ : State) -> State
-  incBall (MkState b) = MkState (b + 2)
+  moveBall : (1 _ : State) -> State
+  moveBall (MkState (MkBall x y) (MkVelocity dx dy))
+    = MkState (MkBall x' y') (MkVelocity dx' dy')
+    where
+      x' : Int
+      x' = x + dx
+
+      y' : Int
+      y' = y + dy
+
+      dx' : Int
+      dx' = if x' > 800 || x' < 0 then (-1 * dx) else dx
+
+      dy' : Int
+      dy' = if y' > 600 || y' < 0 then (-1 * dy) else dy
 
   export
   drawBall : (1 _ : State) -> IO ()
-  drawBall (MkState b) = do
-    fillStyle "black"
-    fillRect 0 0 800 600
-    fillStyle "red"
-    fillRect b 100 5 5
+  drawBall (MkState (MkBall x y) _) = do
+    -- blanks out the screen with black
+    colorRect 0 0 800 600 "black"
+    -- left paddle
+    colorRect 0 210 10 100 "white"
+    -- red ball
+    colorCircle x y 10 "white"
 
 namespace GameLoop
 
@@ -96,4 +151,6 @@ namespace GameLoop
 
 
 main : IO ()
-main = gameLoop 30 initBall incBall drawBall
+main = do
+  consoleLog "Starting..."
+  gameLoop 30 initBall moveBall drawBall
